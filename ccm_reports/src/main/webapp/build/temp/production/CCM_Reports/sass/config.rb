554c384d2c227410d1223f5b1575e7245644a2f1
RamilKhamatyarov <rramil@gmail.com>
2017-01-22 20:27:48 +0300

    Building Ext JS 6 app

---
+++ ccm_reports/src/main/webapp/build/temp/production/CCM_Reports/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\heap\nb_projects\ccm_reports\ccm_reports\src\main\webapp/build/.sass-cache'
---
+++ ccm_reports/src/main/webapp/resources/Readme.md
@@ -0,0 +1,4 @@
+# CCM_Reports/resources
+
+This folder contains resources (such as images) needed by the application. This file can
+be removed if not needed.
