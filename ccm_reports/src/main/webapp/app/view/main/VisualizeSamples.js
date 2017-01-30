bdb6565a15275d249076ecec6d71b7234594c1c1
RamilKhamatyarov <rramil@gmail.com>
2017-01-31 00:44:16 +0300

    Show one report

---
+++ ccm_reports/src/main/webapp/app/view/main/VisualizeSamples.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Ext.onReady(function(){
+//    
+//    Ext.Ajax.request({
+//        xtype: 'visualizesamples', 
+////        url: 'http://192.168.0.32:8080/jasperserver-pro/client/visualize.js',
+////        timeout: 60000,
+//        success: function(response, options){
+//            alert(response.responseText);
+//        },
+//        failure: function(response, options){
+//            alert("Ошибка: " + response.statusText);
+//        }
+//    }); 
+//});
+
+
